1dec1a6a20782dddec45ba2fde62db7d1f091d0f
IeuanHorton <HortonIeuan@gmail.com>
2025-11-30 04:21:32 +0000

    moved terminal display code to it's own file

--- game.cpp
+++ game.cpp
@@ -8,22 +8,14 @@
 
 #include "dice.h"
 #include "bag.h"
+#include "terminalDisplay.h"
 #include <iostream>
 
 Bag bag;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Welcome to														  " << std::endl;	
-	std::cout << "░███████   ░██                      ░██ ░██ 			  " << std::endl;	 
-	std::cout << "░██   ░██                           ░██               " << std::endl;
-	std::cout << "░██    ░██ ░██ ░███████   ░███████  ░██ ░██░███████   " << std::endl; 
-	std::cout << "░██    ░██ ░██░██    ░██ ░██    ░██ ░██ ░██░██    ░██ " << std::endl;
-	std::cout << "░██    ░██ ░██░██        ░█████████ ░██ ░██░██    ░██ " << std::endl;
-	std::cout << "░██   ░██  ░██░██    ░██ ░██        ░██ ░██░██    ░██ " << std::endl; 
-	std::cout << "░███████   ░██ ░███████   ░███████  ░██ ░██░██    ░██ " << std::endl;
-	std::cout << "                                          a dice game " << std::endl;
-	std::cout << "                                                      " << std::endl;
+	displayTitle();
 
 	Dice dice;
 	dice.setDiceFaces(6);
---
+++ terminalDisplay.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+void displayTitle()
+{
+	std::cout << "Welcome to														  " << std::endl;	
+	std::cout << "░███████   ░██                      ░██ ░██ 			  " << std::endl;	 
+	std::cout << "░██   ░██                           ░██               " << std::endl;
+	std::cout << "░██    ░██ ░██ ░███████   ░███████  ░██ ░██░███████   " << std::endl; 
+	std::cout << "░██    ░██ ░██░██    ░██ ░██    ░██ ░██ ░██░██    ░██ " << std::endl;
+	std::cout << "░██    ░██ ░██░██        ░█████████ ░██ ░██░██    ░██ " << std::endl;
+	std::cout << "░██   ░██  ░██░██    ░██ ░██        ░██ ░██░██    ░██ " << std::endl; 
+	std::cout << "░███████   ░██ ░███████   ░███████  ░██ ░██░██    ░██ " << std::endl;
+	std::cout << "                                          a dice game " << std::endl;
+	std::cout << "                                                      " << std::endl;
+}
